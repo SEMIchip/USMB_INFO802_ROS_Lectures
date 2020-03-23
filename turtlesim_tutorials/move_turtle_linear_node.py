b07f01da02f1efaa23fa8d96c4f6db68cc8fa97c
Luc Marechal <36209435+LucMarechal@users.noreply.github.com>
2020-03-23 09:17:41 +0100

    Add files via upload

--- turtlesim_tutorials/move_turtle_linear_node.py
+++ turtlesim_tutorials/move_turtle_linear_node.py
@@ -1,23 +1,27 @@
+#!/usr/bin/env python
 
 import rospy
-XXXXXXXXXXXX  # import Twist
+XXXXXXXXXXXX   # import Twist
 
 def move_turtle():
 
-     # Create a publisher to "talk" to Turtlesim 
+    # Initialize node
+    XXXXXXXXXXXX
+   
+    # Create a publisher to "talk" to Turtlesim 
     pub = XXXXXXXXXXXX
      
-    # Create a Twist message and add linear x and angular z values
+    # Create a Twist message and change linear x value
     vel = Twist()
-    vel.linear.x = 1.0
+    vel.linear.x = 1.0  # Move along the x axis only
 
     # Save current time and set publish rate at 10 Hz
-    now = rospy.Time.now()
+    tStart = rospy.Time.now()
     rate = rospy.Rate(10)
 
     # For the next 6 seconds publish vel move commands to Turtlesim
-    while rospy.Time.now() < now + rospy.Duration.from_sec(6):
-        XXXXXXXXXXXX # publish velocity command to the Turtlesim
+    while rospy.Time.now() < tStart + rospy.Duration.from_sec(6):
+        XXXXXXXXXXXX  # publish velocity command to the Turtlesim
         rate.sleep()
 
           
